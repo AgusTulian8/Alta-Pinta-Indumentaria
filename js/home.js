2e4476aff64fd902eddb6308d7d35076c5a559ac
agustin <agustulian.1071@gmai.com>
2024-05-05 11:52:51 -0300

    5 de mayo

--- js/home.js
+++ js/home.js
@@ -2,3 +2,15 @@ const prendas = JSON.parse(localStorage.getItem("productos")) ||[]
 
 prendas.forEach(prenda=> document.write (prenda.titulo +"\n"))
 
+// crear carrito de compras:
+//poner ids a las etiquetas html (o usar las clases del mismo)
+// importar los ids en js con get element by id o query selector
+//escuchar el boton de agregar al carrito
+//pintar el producto en la tabla usando el evento del boton
+
+
+
+
+
+//
+//
